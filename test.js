2f3030152a1372b3789d6eec0f90fe8fdcc1a072
Robin Larson <robilars@amazon.com>
2019-07-21 15:55:36 -0700

    Upgraded `sum` dependency to fix math bug

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const mean = require('./index')
 
 assert.strictEqual(mean([1,2,3,4]), 2.5,'Average of 1,2,3,4 does not equal 2.5')
 
-assert.strictEqual(mean([1.1,2.3,3.1,4]), 2.5,'Average is of 1.1,2.3,3.1,4 does not equal 2.5')
+assert.strictEqual(mean([1.1,2.3,3.1,4]), 2.625,'Average is of 1.1,2.3,3.1,4 does not equal 2.625')
 
